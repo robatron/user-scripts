a837c6a2d045b451bc0aca9c292b0fd830a16da4
robmc <robmc@zillow.com>
2024-03-04 12:15:17 -0800

    Add slack-browser-redirect script

---
+++ slack-browser-redirect.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Slack Browser Redirect
+// @version      0.0.1
+// @description  Always open Slack message links in browser (never the app)
+// @author       robert.mcgui@gmail.com
+// @match        https://zillowgroup.slack.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @sandbox      JavaScript
+// ==/UserScript==
+
+const consoleFn = (fn, ...args) => console[fn]('[SBR]', ...args);
+const log = (...args) => consoleFn('log', ...args);
+const info = (...args) => consoleFn('info', ...args);
+const debug = (...args) => consoleFn('debug', ...args);
+
+function main() {
+    log('Starting Slack Browser Redirect (Tampermonkey)');
+
+    const isDryRun = document.location.hash.includes('dryrun');
+
+    const redirectLoadingMessageLinks = document.querySelectorAll(
+        '.p-ssb_redirect__loading_messages a',
+    );
+    const openInBrowserLink = [...redirectLoadingMessageLinks].find((el) =>
+        el.getAttribute('href').includes('/messages/'),
+    );
+
+    if (openInBrowserLink) {
+        log(
+            'Found open-in-browser link. Simulating click on ...',
+            openInBrowserLink,
+        );
+
+        if (isDryRun) {
+            log('Dry run 🌵 - Skipping');
+            return;
+        }
+
+        openInBrowserLink.click();
+    } else {
+        log('No open-in-browser link found 🫤 - Skipping');
+    }
+}
+
+main();
