c5d88a2ce06df6ca9365ece3bcbb569ebae539a7
robmc <robmc@zillow.com>
2024-06-06 14:05:47 -0700

    Blind quick commit

--- src/slack-abs-timestamps.user.js
+++ src/slack-abs-timestamps.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Slack Absolute Timestamps
-// @version      0.1.0
+// @version      0.1.1
 // @description  Replace messages' relative timestamps with absolute ones
 // @author       robert.mcgui@gmail.com
 // @homepage     https://github.com/robatron/user-scripts/
