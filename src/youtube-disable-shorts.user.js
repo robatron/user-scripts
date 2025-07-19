70cdca8824878d5abbf218d5c6e80717438881eb
robmc <robmc@zillowgroup.com>
2025-07-19 11:10:43 -0700

    Update youtube-disable-shorts script to version 0.0.2, enhance logging, and improve navigation handling for Shorts pages.

--- src/youtube-disable-shorts.user.js
+++ src/youtube-disable-shorts.user.js
@@ -1,8 +1,39 @@
 // ==UserScript==
 // @name         Block YouTube Shorts Playback
-// @version      0.0.1
+// @version      0.0.2
 // @description  Disables and strips Shorts from YouTube
-// @match        *://www.youtube.com/shorts/*
-// @run-at       document-start
+// @author       robert.mcgui@gmail.com
+// @homepage     https://github.com/robatron/user-scripts/
+// @downloadURL  https://github.com/robatron/user-scripts/raw/main/src/youtube-disable-shorts.user.js
+// @updateURL    https://github.com/robatron/user-scripts/raw/main/src/youtube-disable-shorts.user.js
+// @grant        none
+// @icon         https://www.youtube.com/favicon.ico
+// @match        https://www.youtube.com/*
 // ==/UserScript==
-window.location.replace('https://www.youtube.com');
+
+// Logging
+const consoleFn = (fn, ...args) => console[fn]('[YT]', ...args);
+const log = (...args) => consoleFn('log', ...args);
+const info = (...args) => consoleFn('info', ...args);
+const debug = (...args) => consoleFn('debug', ...args);
+
+function main() {
+    log('Starting YouTube Disable Shorts (Tampermonkey)');
+
+    // First, check if we've directly navigated to a shorts page
+    if (window.location.pathname.includes('/shorts/')) {
+        info('Directly navigated to a shorts page');
+        window.location.replace('https://www.youtube.com');
+        return;
+    }
+
+    // Next, watch for changes to location.pathname
+    window.addEventListener('popstate', (event) => {
+        info('Soft navigation to a shorts page. Event:', event);
+        if (event.state?.pathname.includes('/shorts/')) {
+            window.location.replace('https://www.youtube.com');
+        }
+    });
+}
+
+main();
