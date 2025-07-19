8289db6be1568d2c63e1fb1c94f5695b3948a4b4
robmc <robmc@zillowgroup.com>
2025-07-19 10:57:02 -0700

    Add youtube-disable-shorts script

---
+++ src/youtube-disable-shorts.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Block YouTube Shorts Playback
+// @version      0.0.1
+// @description  Disables and strips Shorts from YouTube
+// @match        *://www.youtube.com/shorts/*
+// @run-at       document-start
+// ==/UserScript==
+window.location.replace('https://www.youtube.com');
